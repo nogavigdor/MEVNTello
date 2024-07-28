15cfba15b0ba148fc4d6b8b5459e180e30662142
Noga Vigdor <noga.vigdor@gmail.com>
2024-07-28 21:24:04 +0200

    added task interface

---
+++ src/interfaces/ITask.ts
@@ -0,0 +1,16 @@
+import { TeamMember } from './ITeamMember';
+import SubTask from './ISubTask';
+//Interface for the tast object
+export interface Task {
+    _id: string;
+    listId: string;
+    name: string;
+    description: string;
+    assignedMembers: TeamMember[]; // Array of User IDs
+    hoursAllocated: number;
+    hoursUsed: number;
+    status: 'todo' | 'inProgress' | 'done';
+    subTasks: SubTask[];
+    createdAt?: Date;
+    updatedAt?: Date;
+} 
