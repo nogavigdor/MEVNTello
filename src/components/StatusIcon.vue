3037b7ea4169a6baa0adc293d751bab83dfeb079
Noga Vigdor <noga.vigdor@gmail.com>
2024-08-06 23:43:40 +0200

    status of project is presented by an appropriate icon

--- src/components/StatusIcon.vue
+++ src/components/StatusIcon.vue
@@ -1,16 +1,61 @@
 <template>
-    <div class="flex space-x-4 overflow-x-auto">
-        <div v-for="(step, index) in steps" :key="index" class="bg-gray-100 rounded-lg p-4 w-80">
-        {{step}}
-        </div>
+    <div :class="statusClass" :title="tooltip">
+      <font-awesome-icon :icon="icon" />
     </div>
-</template>
-<script setup lang="ts">
-import { ref, defineProps } from 'vue';
-
-const props = defineProps<{ steps: string[] }>();
-
-
-</script>
-<style scoped>
-</style>
+  </template>
+  
+  <script setup lang="ts">
+  import { defineProps, computed } from 'vue';
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+  import { library } from '@fortawesome/fontawesome-svg-core';
+  import { faCheckCircle, faExclamationCircle, faHourglassHalf, faCircle } from '@fortawesome/free-solid-svg-icons';
+  
+  // Add the icons to the library
+  library.add(faCheckCircle, faExclamationCircle, faHourglassHalf, faCircle);
+  
+  const props = defineProps<{ status: string }>();
+  
+  const statusClass = computed(() => {
+    switch (props.status) {
+      case 'Completed':
+        return 'text-green-500';
+      case 'inProgress':
+        return 'text-yellow-500';
+      case 'overdue':
+        return 'text-red-500';
+      default:
+        return 'text-gray-500';
+    }
+  });
+  
+  const icon = computed(() => {
+    switch (props.status) {
+      case 'Completed':
+        return 'check-circle';
+      case 'inProgress':
+        return 'hourglass-half';
+      case 'overdue':
+        return 'exclamation-circle';
+      default:
+        return 'circle';
+    }
+  });
+  
+  const tooltip = computed(() => {
+    switch (props.status) {
+      case 'Completed':
+        return 'This project is completed.';
+      case 'inProgress':
+        return 'This project is in progress.';
+      case 'overdue':
+        return 'This project is overdue.';
+      default:
+        return 'Status unknown.';
+    }
+  });
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
