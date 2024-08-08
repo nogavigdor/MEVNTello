9a5aa982583bbbabf42ed3dc1eb1c0b0987c21f3
Noga Vigdor <noga.vigdor@gmail.com>
2024-08-09 00:49:59 +0200

    removed uneeded step props

--- src/components/AdminOptions.vue
+++ src/components/AdminOptions.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="flex space-x-4 overflow-x-auto">
-        <div v-for="(step, index) in steps" :key="index" class="bg-gray-100 rounded-lg p-4 w-80">
-        {{step}}
-        </div>
+  
+       
     </div>
 </template>
 <script setup lang="ts">
 import { ref, defineProps } from 'vue';
 
-const props = defineProps<{ steps: string[] }>();
+
 </script>
