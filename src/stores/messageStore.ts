133962de48a4bef5afd5433b5de057b7dfda832e
Noga Vigdor <noga.vigdor@gmail.com>
2024-08-09 19:16:58 +0200

    added setTimeout to clear message

--- src/stores/messageStore.ts
+++ src/stores/messageStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import { Message, MessageType } from '@/interfaces/IMessage';
+import { set } from 'lodash';
 
 //Store for managing messages - error, warning, success
 export const useMessageStore = defineStore('message', () => {
@@ -9,6 +10,9 @@ export const useMessageStore = defineStore('message', () => {
   // Set the message.
   function setMessage(text: string, type: MessageType) {
     message.value = { text, type };
+    setTimeout(() => {
+      clearMessage();
+    }, 5000);
   }
 
   // Clear the message.
