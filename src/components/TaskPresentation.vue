a8a1fc3ad341cf4d6e08f39a8bef37e28247532d
Noga Vigdor <noga.vigdor@gmail.com>
2024-08-10 20:43:11 +0200

    added input to allocated hours and assigned members in case management stage in new project creation is not finalized - admin and leader persmissions only

--- src/components/TaskPresentation.vue
+++ src/components/TaskPresentation.vue
@@ -21,7 +21,11 @@
             <span class="ml-2">Done</span>
           </label>
         </div>
-      <p class="mb-2">Allocated Hours: {{ task.hoursAllocated }}</p>
+      <p class="mb-2">Allocated Hours: <input v-if="isAdmin || isLeader" v-model="task.hoursAllocated" @change="allocatedHours" type="number" /> <span v-else >{{ task.hoursAllocated }}</span></p>
+      <div v-if="(isAdmin || isLeader) && task.assignedMembers.length === 0" class="mb-2">
+        <AssignMembers v-model="selectedMembers" :projectTeamMembers="projectTeamMembers" />
+        <button @click="assignMembers">Assign Members</button>
+      </div>
       <div v-for="member in task.assignedMembers" :key="member._id" class="mb-2">
       <p class="mb-2">{{ member.username }}'s Used Hours: 
         <span v-if="amImemberInTask(member)">
@@ -48,9 +52,11 @@
   import { useProjectStore } from '@/stores/projectStore';
   import { AssignedMember } from '@/interfaces/IAssignedMember';
   //handles the debouncing of the input field
-  import { debounce} from 'lodash';
+  import { assign, debounce} from 'lodash';
   import EditTaskModal from './EditTaskModal.vue';
   import { TeamMember } from '@/interfaces/ITeamMember';
+  import AssignMembers from './AssignMembers.vue';
+import { getMemberName } from '@/utils/getMemberName';
   
   const props = defineProps<{ task: Task, projectId: string, isLeader: boolean, projectTeamMembers: TeamMember[] }>();
   
@@ -65,16 +71,32 @@
   const project = computed<Project | undefined>(() => projectStore.getProjectById(props.projectId));
 
   const showEditModal = ref(false);
+
+  const selectedMembers = ref<string[]>([]);
   
   const isTaskMember = (task: Task) => {
     return props.task.assignedMembers.some(member => member._id === userStore.user?._id);
+  };
+
+  const assignMembers = async () => {
+    const assignedMembers: AssignedMember[] = selectedMembers.value.map(memberId => {
+      const member = props.projectTeamMembers.find(member => member._id === memberId);
+      return member ? { _id: member._id, username: getMemberName(memberId), role: member.role, usedHours: 0 } : { _id: memberId, username: 'Unknown', role: 'member', usedHours: 0 };
+    });
+    await tasksStore.updateTask(props.task._id, { assignedMembers });
   };
   
   const updateTask = async (updatedTask: Partial<Task>) => {
   await tasksStore.updateTask(props.task._id, updatedTask);
   showEditModal.value = false;
 };
-  
+
+//debounce the update of allocated hours for each member to avoid multiple requests
+const allocatedHours = debounce(async () => {
+  await tasksStore.updateTask(props.task._id, { hoursAllocated: props.task.hoursAllocated });
+}, 300);
+
+//check if the user's used hours is above 0 and the task status is todo, then update the task status to inProgress
 const updateMemberUsedHours = debounce(async (memberId: string, usedHours: number) => {
   await tasksStore.updateMemberUsedHours(props.task._id, memberId, usedHours);
   if (usedHours > 0 && props.task.status === 'todo') {
@@ -95,10 +117,14 @@ const memberProgress = (member: AssignedMember) => {
   return Math.min((member.usedHours / props.task.hoursAllocated) * 100, 100);
 };
 
+
+//calculate the total used hours of the task by all the assigned members
 const totalUsedHours = computed(() => {
   return props.task.assignedMembers.reduce((acc, member) => acc + member.usedHours, 0);
 });
 
+
+//calculate the total progress of the task
 const totalProgress = computed(() => {
   //in casa the hours allocated are 0, we return 0 to avoid division by 0
   return props.task.hoursAllocated > 0 
